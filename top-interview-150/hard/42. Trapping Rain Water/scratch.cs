8268158f9b6c655ceb8a88325fc0194e408624f0
Ryan Waszak <rwaszak@tesla.com>
2024-06-27 09:20:40 -0700

    resolve plateau peaks/valleys that were being assigned as steps

--- top-interview-150/hard/42. Trapping Rain Water/scratch.cs
+++ top-interview-150/hard/42. Trapping Rain Water/scratch.cs
@@ -3,7 +3,7 @@ public class Solution {
     private const string _VALLEY = "V";
     private const string _STEP = "S";
     private string _currentLimitProperty = "";
-    private int _currentLimitIndex = -1;
+    private int _currentLimitIndex = 0;
     private int _currentLimitHeight = -1;
     private string[] _propertiesArray = null;
 
@@ -52,7 +52,7 @@ public class Solution {
         {
             Console.WriteLine("Property for index " + i + " is: " + _propertiesArray[i]);
         }
-        
+
         return -1;
     }
 
@@ -61,15 +61,24 @@ public class Solution {
     {
         for (var i = _currentLimitIndex + 1; i < height.Length; i++)
         {
+            var current = height[i];
+
             if(i == height.Length - 1)
             {
                 _propertiesArray[i] = _VALLEY;
                 _currentLimitProperty = _VALLEY;
                 _currentLimitIndex = i;
+                _currentLimitHeight = current;
+
+                while(i >= 0 && height[i] == _currentLimitHeight)
+                {
+                    _propertiesArray[i] = _VALLEY;
+                    i--;
+                }
+
                 break;       
             }
 
-            var current = height[i];
             var next = height[i + 1];
             bool isValley = CurrentIsValley(current, next);
 
@@ -87,6 +96,16 @@ public class Solution {
                 _currentLimitProperty = _VALLEY;
                 _currentLimitIndex = i;
                 _currentLimitHeight = current;
+
+                while(i >= 0 && height[i] == _currentLimitHeight)
+                {
+                    _propertiesArray[i] = _VALLEY;
+                    i--;
+                }
+                // Thought:
+                // Traverse backwards now and assign any left neighbors with same height as a valley
+                // since at this point they would have been assigned step
+
                 break;                    
             }
         }
@@ -97,15 +116,24 @@ public class Solution {
     {
         for (var i = _currentLimitIndex + 1; i < height.Length; i++)
         {
+            var current = height[i];
+
             if(i == height.Length - 1)
             {
                 _propertiesArray[i] = _PEAK;
                 _currentLimitProperty = _PEAK;
                 _currentLimitIndex = i;
+                _currentLimitHeight = current;
+
+                while(i >= 0 && height[i] == _currentLimitHeight)
+                {
+                    _propertiesArray[i] = _PEAK;
+                    i--;
+                }
+
                 break;
             }
 
-            var current = height[i];
             var next = height[i + 1];
 
             bool isPeak = CurrentIsPeak(current, next);
@@ -124,10 +152,23 @@ public class Solution {
                 _currentLimitProperty = _PEAK;
                 _currentLimitIndex = i;
                 _currentLimitHeight = current;
+
+                // Thought:
+                // Traverse backwards now and assign any left neighbors with same height as a peak
+                // since at this point they would have been assigned step
+                while(i >= 0 && height[i] == _currentLimitHeight)
+                {
+                    _propertiesArray[i] = _PEAK;
+                    i--;
+                }
+
                 break;
             }
         }
     }
+
+
+    // TODO: These don't solve for when the valley or peak is a plateau, i.e. next == current
 
     private bool CurrentIsPeak(int current, int next)
     {
@@ -138,6 +179,4 @@ public class Solution {
     {
         return next > current;
     }
-
-
 }
